7a46f90a0e9f7135c446d981db0169ceee93e120
Oleg Smith <olegsmith@gmail.com>
2013-08-09 00:00:02 +0400

    refactoring breadcrumb

--- ng-app/modules/core/breadcrumbs/breadcrumbsCtrl.js
+++ ng-app/modules/core/breadcrumbs/breadcrumbsCtrl.js
@@ -5,5 +5,13 @@ angular.module('core')
             $scope.$on('page:configChanged', function() {
                 $scope.breadcrumbs = $pageConfig.current.breadcrumbs;
             });
+
+            $scope.isHref = function (index) {
+            	var res = true;
+            	if ((index === ($scope.breadcrumbs.length-1)) || ($scope.breadcrumbs[index].url === '')) {
+            		res=false;
+            	}
+            	return res;
+            }
         }
     ]);
